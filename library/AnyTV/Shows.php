581b89f0eecebd9d9fd2332282da33eba27d73c9
adin234 <aidnbautista@gmail.com>
2014-05-30 13:59:20 +0800

    add shows

--- library/AnyTV/Shows.php
+++ library/AnyTV/Shows.php
@@ -7,7 +7,8 @@ class AnyTV_Shows
 		$options = $options = XenForo_Application::get('options');
 		$response->templateName = 'anytv_shows_page';
 		$response->params['games'] = AnyTV_Games::getGames();
-		$response->params['channel'] = $options->ShowsChannel;
+        $response->params['option'] = array('profile' => 'http://www.facebook.com/mcnfreedom');¬
+        $response->params['channel'] = $options->ShowsChannel;
 		$response->params['playlist'] = $options->ShowsPlaylist;
 	}
 }
