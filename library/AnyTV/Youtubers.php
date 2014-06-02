e2c19021e86b051a5bea0e2311d5e63e6f2b6c5b
adin234 <aidnbautista@gmail.com>
2014-06-02 16:58:04 +0800

    youtubers

--- library/AnyTV/Youtubers.php
+++ library/AnyTV/Youtubers.php
@@ -5,8 +5,11 @@ class AnyTV_Youtubers
 	public static function resposeLayout(XenForo_ControllerPublic_Page $controller, XenForo_ControllerResponse_View $response)
 	{
 		// fetch all recent members who registered
+
 		$userModel = $controller->getModelFromCache('XenForo_Model_User');
-		$response->params['users'] = $userModel->getLatestUsers(array(), array('limit' => 16));
+		$fieldsModel =  XenForo_Model::create('AnyTV_Models_CustomUserFieldModel');
+		$response->params['users'] = $fieldsModel->getFieldValuesByFieldId('youtube_id');
+		$response->params['users'] = $response->params['users']['youtube_id'];
         $options = $options = XenForo_Application::get('options');
         $response->params['option'] = array('profile' => $options->facebookLink);
 		$response->templateName = 'youtubers_list_page';
