90091cf18035b3a46cd945544bd78741fdade73c
adin234 <aidnbautista@gmail.com>
2014-06-04 19:13:09 +0800

    position

--- library/EWRporta/Block/RecentFeatures.php
+++ library/EWRporta/Block/RecentFeatures.php
@@ -7,12 +7,13 @@ class EWRporta_Block_RecentFeatures extends XenForo_Model
 		$options['forum'] = implode(',', $this->getModelFromCache('EWRporta_Model_Promotes')->getPromoteForums());
 
 		$news = $this->_getDb()->fetchAll("
-			SELECT xf_thread.*, xf_post.message, xf_attachment.*, xf_attachment_data.*,
+			SELECT xf_thread.*, xf_post.message, xf_attachment.*, xf_attachment_data.*, xf_post_file_value.field_value as header_location
 				IF(EWRporta_promotes.promote_date IS NULL, xf_thread.post_date, EWRporta_promotes.promote_date) AS promote_date
 			FROM xf_thread
 				INNER JOIN xf_post ON (xf_post.post_id = xf_thread.first_post_id)
 				INNER JOIN xf_attachment ON (xf_attachment.content_id = xf_thread.first_post_id AND xf_attachment.content_type = 'post')
 				INNER JOIN xf_attachment_data ON (xf_attachment_data.data_id = xf_attachment.data_id AND xf_attachment_data.filename = ? AND xf_attachment_data.thumbnail_width > 0)
+				INNER JOIN xf_post_field_value ON (xf_post_field_value.post_id = xf_post.post_id AND xf_post_field_id = 'headerLocation')
 				LEFT JOIN EWRporta_promotes ON (EWRporta_promotes.thread_id = xf_thread.thread_id)
 			WHERE (xf_thread.node_id IN (".$options['forum'].") OR EWRporta_promotes.promote_date < ?)
 				AND xf_thread.discussion_state = 'visible'
